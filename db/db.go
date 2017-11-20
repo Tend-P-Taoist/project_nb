0e5ff6d49bfacc4b2934b8490081e0d0f570c7cd
pansifan0525@163.com <pansifan845514>
2017-11-20 17:18:53 +0800

    models

--- db/db.go
+++ db/db.go
@@ -3,6 +3,7 @@ package db
 import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
+
 )
 
 var DB gorm.DB
--- main.go
+++ main.go
@@ -5,13 +5,10 @@ import (
 	"github.com/gin-gonic/gin"
 	)
 
-type cc struct {
-	a string
-}
 func main() {
 
 	gin.SetMode(gin.DebugMode)
 	engine := gin.Default()
-	
+
 	engine.Run(":8888")
 }
